2d12266eb5533e9c8d896e25fefdd8e19b0ede8b
Happi Kaur <38865414+happikaur@users.noreply.github.com>
2022-01-25 20:19:57 +0000

    Separating buttons to numbers and operators. And WIP of getTotal function of equals.

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
-// Number symbol number
+const numbers = document.querySelectorAll('.number');
+const input = document.querySelector('.input');
+const equal = document.querySelector('.equals')
+const operators = document.querySelectorAll('.operator');
 
+let firstNumber;
+let secondNumber;
+let total;
+let operator;
 
 // On-click displays the value
-const buttons = document.querySelectorAll('.button');
-const input = document.querySelector('.input');
-buttons.forEach((button) => {
+numbers.forEach((button) => {
   button.addEventListener("click", (event) => {
     event.preventDefault();
-    console.log(event.target.textContent)
-    input.innerHTML = input.innerHTML + event.target.textContent;
+    console.log(event.target.innerHTML)
+    // Show the input
+    input.innerHTML += event.target.innerHTML;
   });
 });
+
+operators.forEach((symbol) => {
+  symbol.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(event.target.innerHTML)
+    // Show the input
+    firstNumber = input.value;
+    operator = event.target.innerHTML;
+    input.innerHTML += event.target.innerHTML;
+    // operator = input.value
+  });
+});
+
 console.log(input);
-// Show the input
+
 // When pressed "=" take the input/value (need to use querySelector)
+equal.addEventListener("click", (event) => {
+  getTotal(input.value);
+});
+
 // Make a function of calculation with input string
+const getTotal = (inputText) => {
+ // 5+5
+ // number,symbol,number
+ // switch on symbol
+  // do the calc in javascript
+  // return the result
+};
+
 // Change numbers to number & symbols stays as string
  
