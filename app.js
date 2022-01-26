5b3f3df75d77b56c0b974678bdc18a0a7270889e
Happi Kaur <38865414+happikaur@users.noreply.github.com>
2022-01-26 23:38:46 +0000

    Made the calculation for % and WIP for the +/-

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const numbers = document.querySelectorAll('.number');
 const input = document.querySelector('.input');
-const equal = document.querySelector('.equals')
+const equal = document.querySelector('.equals');
 const operators = document.querySelectorAll('.operator');
 const cancel = document.querySelector('.clear');
+const plusMinus = document.querySelector('.plus-minus');
 
 let firstNumber;
 let secondNumber = '';
-let operator;
-let total;
+let operator = '';
+// let operator = [];
+let total = 0;
+let isPlusMinus = false;
 
 // On-click displays the value
 numbers.forEach((button) => {
-  button.addEventListener("click", (event) => {
+  button.addEventListener('click', (event) => {
     event.preventDefault();
     console.log(event.target.innerHTML)
     // Show the input
@@ -26,51 +29,93 @@ numbers.forEach((button) => {
 });
 
 operators.forEach((symbol) => {
-  symbol.addEventListener("click", (event) => {
+  symbol.addEventListener('click', (event) => {
     event.preventDefault();
     console.log(event.target.innerHTML)
     // Setting variables 
     firstNumber = input.innerHTML;
-    operator = event.target.innerHTML;
+    // operator.push(event.target.innerHTML);
+    operator = operator + event.target.innerHTML
     // Adding the symbol to the input
     input.innerHTML = input.innerHTML + event.target.innerHTML;
   });
 });
 
-// When pressed "=" take the input/value (need to use querySelector)
-equal.addEventListener("click", () => {
-  total = getTotal(firstNumber, operator, secondNumber);
+// When pressed '=' take the input/value (need to use querySelector)
+equal.addEventListener('click', (event) => {
+  // operator.forEach((symOperator) => {
+    // total = total + getTotal(firstNumber, symOperator, secondNumber);
+  // });
+  total = getTotal(firstNumber, operator, secondNumber)
   input.innerHTML = input.innerHTML + event.target.innerHTML
   input.innerHTML = input.innerHTML + total;
 });
 
 // Make a function of calculation with input string
-const getTotal = (firstNumber, operator, secondNumber) => {
+const getTotal = (firstNum, operator, secondNum) => {
  // Change numbers to number & symbols stays as string
- const intFirstNum = parseFloat(firstNumber);
- const intSecondNum = parseFloat(secondNumber);
+ let intFirstNum = parseFloat(firstNum);
+ let intSecondNum = parseFloat(secondNum);
+
+ if (isPlusMinus) {
+   return 
+ }
 
  // switch on symbol
  // do the calc in javascript
  // return the result
  switch(operator) {
-   case '+':
-     return intFirstNum + intSecondNum;
-   case '-':
-     return intFirstNum - intSecondNum;
-   case '÷':
-     return (intFirstNum / intSecondNum).toFixed(5);
-   case 'x':
-     return intFirstNum * intSecondNum;
+    case '+':
+      return intFirstNum + intSecondNum;
+    case '-':
+      return intFirstNum - intSecondNum;
+    case '÷':
+      return (intFirstNum / intSecondNum).toFixed(5);
+    case 'x':
+      return intFirstNum * intSecondNum;
+    case '%': 
+    // {
+    //   if (intFirstNum && intSecondNum) {
+    //     return (intSecondNum / 100 * intFirstNum);
+    //   } else if (intFirstNum && !intSecondNum) {
+        return (intFirstNum / 100);
+    //   }
+    // }  
+    //  % after other operators
+    case '+%':
+      return intFirstNum + (intSecondNum / 100 * intFirstNum);
+    case '-%':
+      return intFirstNum - (intSecondNum / 100 * intFirstNum);
+    case '÷%':
+      return (intFirstNum / (intSecondNum / 100)).toFixed(5);
+    case 'x%':
+      return intFirstNum * (intSecondNum / 100 );
+    //  % before other operators
+    case '%+':
+      return (intFirstNum / 100) + intSecondNum;
+    case '%-':
+      return (intFirstNum / 100) - intSecondNum;
+    case '%÷':
+      return ((intFirstNum / 100) / intSecondNum).toFixed(5);
+    case '%x':
+      return (intFirstNum / 100) * intSecondNum;
  }  
 };
 
+plusMinus.addEventListener('click', (event) => {
+  isPlusMinus = true;
+});
+
 //  Cancel function
-cancel.addEventListener("click", (event) => {
+cancel.addEventListener('click', (event) => {
   event.preventDefault();
   // Show the input
-  firstNumber = "";
-  secondNumber = "";
-  input.innerHTML = "";
+  firstNumber = '';
+  secondNumber = '';
+  operator = '';
+  // operator = [];
+  total = 0;
+  input.innerHTML = '';
+  isPlusMinus = false;
 }); 
 
